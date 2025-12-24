3c4965901be29d3f6af130d1a226cc4c7ccf9bad
unknown <aflahkondoth@gmail.com>
2025-12-24 19:44:16 +0530

    up url

--- scripts/testMockPayment.js
+++ scripts/testMockPayment.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:8000/api';
+const API_BASE = 'http://https://movie-bd-ashen.vercel.app/api';
 
 // Test mock payment flow
 async function testMockPayment() {
